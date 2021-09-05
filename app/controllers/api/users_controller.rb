743fd998b3b7d5a033fbee6547c3ebcf99866ecb
cgradeff <64032507+cgradeff@users.noreply.github.com>
2021-09-05 18:25:38 -0400

    session and user controller done

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,27 @@
+class Api::UsersController < ApplicationController
+    def show 
+        @user = User.find_by(email: params[:email])
+        render :show
+    end
+    
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    # def new 
+    #     @user = User.new
+    # end
+
+    private 
+    
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
---
+++ db/migrate/20210905162514_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :location, null: false
+      t.string :bio
+      t.string :password_digest, mull: false
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
